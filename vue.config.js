5c6d2e7b1265d12edf098cec6a9bd63d0a736512
reizen-desu <nokiaq80@gmail.com>
2022-09-06 01:33:51 +0200

    Update for production & local dev

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-mozambique-weather'
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-mozambique-weather/" : "/"
 })
