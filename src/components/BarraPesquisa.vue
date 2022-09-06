41526206d82eccc6a6812f6bf19da97336a7f6bf
reizen-desu <nokiaq80@gmail.com>
2022-09-06 04:17:48 +0200

    Add Barra de Pesquisa

---
+++ src/components/BarraPesquisa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Barra de pesquisa</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BarraPesquisa",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
