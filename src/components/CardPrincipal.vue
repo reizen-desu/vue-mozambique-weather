ee4ffc7bc6fd6155890250b614495820703e74fd
reizen-desu <nokiaq80@gmail.com>
2022-09-06 04:17:31 +0200

    Add Card Principal

---
+++ src/components/CardPrincipal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Main card</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CardPrincipal",
+    }
+</script>
+
+<style scoped>
+
+</style>
