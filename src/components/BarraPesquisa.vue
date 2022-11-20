58c6acab754e89703f4c77f1c646774f70fa27f3
reizen-desu <nokiaq80@gmail.com>
2022-11-20 18:21:09 +0200

    feat: implements search using v-select component

--- src/components/BarraPesquisa.vue
+++ src/components/BarraPesquisa.vue
@@ -1,10 +1,8 @@
 <template>
-    <div id="principal">
-        <form>
-            <input v-on:click="showProvinces()" type="text" name="pesquisar" id="texto"
-                placeholder="Pesquisar cidade específica">
-            <input type="submit" value="Pesquisar" id="botao">
-        </form>
+    <div id="container">
+        <v-select v-model="selected" :options="provinces" label="name" id="search-text">
+        </v-select>
+        <input type="submit" value="Pesquisar" id="search-button" @click="showProvince()">
     </div>
 </template>
 
@@ -12,38 +10,64 @@
 // import CardPrincipal from './CardPrincipal.vue';
 export default {
     name: "BarraPesquisa",
+    data() {
+        return {
+
+            selected: null,
+            provinces: [
+                'Maputo',
+                'Xai-Xai',
+                'Pemba',
+                'Inhambane',
+                'Chimoio',
+                'Nampula',
+                'Lichinga',
+                'Beira',
+                'Tete',
+                'Quielimane',
+            ],
+        }
+    },
 
     methods: {
-        showProvinces() {
-            console.log("Tenho que adicionar as provincias como sugestao ao clicar na barra...");
+        showProvince() {
+            if (this.selected) {
+                // this.$emit('show-province', this.selected);
+                console.log("Provincia selecionada: " + this.selected);
+            }
+            else {
+                console.log("Nenhuma provincia selecionada");
+                // Sends an alert
+                alert("Choose a province first");
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-#principal {
-    color: white;
+#container {
     text-align: center;
     white-space: nowrap;
-    width: 100%;
     margin: 100px 0 50px;
 }
 
-form input {
-    padding: 15px;
-}
-
-#texto {
+#search-text {
+    display: inline-block;
+    background-color: white;
     width: 40%;
     border-radius: 5px 0 0 5px;
     text-align: center;
+    padding: 5.5px 0;
 }
 
-#botao {
+#search-button {
     border: none;
     background-color: #69465B;
     color: white;
+    font-weight: 600;
     cursor: pointer;
+    padding: 15px;
+    border-radius: 0 5px 5px 0;
 }
 </style>
