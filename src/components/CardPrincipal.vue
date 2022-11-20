f2a0634be0d1201cfce4eeff103f5365c3090b22
reizen-desu <nokiaq80@gmail.com>
2022-11-20 03:32:35 +0200

    refactor: add icons directly to divs

--- src/components/CardPrincipal.vue
+++ src/components/CardPrincipal.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div id="main-card">
         <h2 v-if="dados">{{ dados[0] }}</h2>
         <div id="icon">
-            <div v-if="dados[3] == 'Rain'" id="rain"></div>
-            <div v-if="dados[3] == 'Clouds'" id="clouds"></div>
-            <div v-if="dados[3] == 'Clear'" id="clear"></div>
+            <div v-if="dados[3] == 'Rain'">🌧️</div>
+            <div v-if="dados[3] == 'Clouds'">🌥️</div>
+            <div v-if="dados[3] == 'Clear'">☀️</div>
         </div>
         <h4>Tempo actual: <span>{{ dados[1] }} ºC</span></h4>
     </div>
@@ -17,39 +17,26 @@ export default {
 }
 </script>
 
-<style scoped>
-div {
+<style>
+#main-card {
     margin: 25px 0;
-    border-radius: 15px;
+    border-radius: 10px;
     text-align: center;
     color: white;
     background-color: #4E3863;
-    width: 250px;
 }
 
-h4 {
+#main-card h4 {
     color: #C0C0C0;
 }
 
-h4 span {
+#main-card h4 span {
     color: rgb(59, 118, 122);
 }
 
-#icon {
+#main-card #icon {
     text-align: center;
-    font-size: 100px;
+    font-size: 120px;
     display: block;
-}
-
-#rain::after {
-    content: "🌧️";
-}
-
-#clouds::after {
-    content: "🌥️";
-}
-
-#clear::after {
-    content: "☀️";
 }
 </style>
